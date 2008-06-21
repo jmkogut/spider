138ebb6adbc86d2218dd7cd1361d8b6d66969fed
Joshua Michael Kogut <joshua.kogut@gmail.com>
2008-06-21 15:33:15 -0700

    AND SPIDER WAS BORN

--- Spider.py
+++ Spider.py
@@ -1,45 +1,59 @@
 import re
+import time
 import urllib
 import threading
 
 class Spider(threading.Thread):
     
-    def __init__(self, url, **kwargs):
+    def __init__(self, pool, history, **kwargs):
         threading.Thread.__init__(self)
         
-        self.url = url
+        self.pool = pool
+        self.history = history
+
         self.args = kwargs
         
     def run(self):
-        # Set the current url in the pool to assigned, but no DB (thanks Mike!)
-        # Oh wait, I can't, no DB exists
+        while True:
+            # Set the current url in the pool to assigned, but no DB (thanks Mike!)
+            # Oh wait, I can't, no DB exists
+    
+            # urllib that source straight from a url
+            self.url = self.pool.pop()
+            print "Spidering %s" % self.url
+            source = urllib.urlopen(self.url).read()
+    
+            # From said source, capture all url-like strings
+            urls = self.args["filter"].findall(source)
+            urls.sort()
+    
+            # Remove duplicate urls from list
+            for url in urls:
+                while urls.count(url) != 1:
+                    urls.remove(url)
+            
+            # Add absolute URLs to the pool
+            for url in urls:
+                if url.startswith('http') and url not in pool:
+                    self.pool.append(url)
+    
+            self.history.append(self.url)
+    
+            time.sleep(.2)
 
-        # urllib that source straight from a url
-        source = urllib.urlopen(self.url).read()
 
-        # From said source, capture all url-like strings
-        urls = self.args["filter"].findall(source)
-        urls.sort()
-
-        # Remove duplicate urls from list
-        for url in urls:
-            while urls.count(url) > 1:
-                urls.remove(url)
-        
-        # Remove non-absolute urls (because.. I only want external links for right now)
-        for url in urls:
-            if url.startswith('/'):
-                urls.remove(url)
-        
-        print urls
 
 
 if __name__ == '__main__':
 
-    pool = []
+
+#    global pool
+#    global history
+
+    pool = ['http://digg.com']
     history = []
 
     regex = re.compile('href="([^\"]+)"')
 
-    s = Spider('http://digg.com/', pool=pool, history=history, filter=regex)
+    s = Spider(pool, history, filter=regex)
     s.start()
