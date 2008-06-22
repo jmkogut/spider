4e931c22b68b048b31a8693d24c6bceace89227c
Joshua Michael Kogut <joshua.kogut@gmail.com>
2008-06-21 19:21:31 -0700

    Started configuration file

---
+++ Configuration.py
@@ -0,0 +1,19 @@
+ignore_suffix = [
+    '.png',
+    '.jpg',
+    '.jpeg',
+    '.ico',
+    '.tiff',
+    '.gif',
+    '.swf',
+    '.mov',
+    '.wmv',
+    '.mp3',
+    '.avi',
+]
+
+ignore_domain = []
+
+optimize = False
+threadpause = .5
+maxthreads = 10
